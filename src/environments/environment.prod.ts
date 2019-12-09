7d9627645cd97c858fd458ace39c4d9aac16f955
Rodrigo Burciaga <rodrigo-burciaga@localhost.localdomain>
2019-12-09 14:48:37 -0600

    testing jenkins

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoicmJ1cmNpYWdhIiwiYSI6ImNqdzQzeWtreTFlZmYzenFrbGkwYWhnN3EifQ.nDE_UPSdca5lAu_P6kNuww'
+  },
+  config: {
+    urlServer: 'http://localhost:3000',
+    apiGeolocalication: '/apiGeoLocalization',
+    endpointGeoJSON: '/geoJSON'
+  }
 };
