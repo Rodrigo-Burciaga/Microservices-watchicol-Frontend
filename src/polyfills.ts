5222704d178af5a7adf4bc5726d785ac296af57e
Rodrigo Burciaga <rodrigo-burciaga@localhost.localdomain>
2019-12-09 15:25:05 -0600

    new files

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -77,5 +77,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
+const GLOBAL = 'global';
 (window as any)['global'] = window;
